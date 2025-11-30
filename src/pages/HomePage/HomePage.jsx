2e886f6e03e0a7ccfdf34cb2ca44975917de37a2
norowrta <m.bohatovv@gmail.com>
2025-11-30 22:40:43 +0100

    initial commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,12 @@
+import Footer from "../../Components/Footer/Footer";
+
 const HomePage = () => {
   return (
     <div>
-        <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to the Home Page</h1>
+      <Footer></Footer>
     </div>
-    )
+  );
 }
 
 export default HomePage
