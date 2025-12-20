dd131268567c6e49205cec06bb5218f1558ad30c
KarpovEvguen <karpov.zhenya26@gmail.com>
2025-12-20 10:18:43 +0100

    add fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
 import HomePage from './pages/HomePage/HomePage.jsx'
-import Banner1 from './Components/Banners/banner1.jsx'
-import Banner2 from  './Components/Banners/banner2.jsx'
 
 
 function App() {
@@ -9,8 +7,7 @@ function App() {
 
   return (
     <>
-       <Banner1/>
-      <Banner2/>
+    <HomePage />
     </>
   )
 }
