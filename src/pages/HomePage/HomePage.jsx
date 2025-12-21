fba2090143c431f461fb61ba38e31df67fca76c5
Garber1329 <91221454+Garber1329@users.noreply.github.com>
2025-12-21 11:22:53 +0200

    add module to swiper

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,6 @@ import Footer from "../../Components/Footer/Footer.jsx";
 import ProductCardList from "../../Components/ProductCard/ProductCardList.jsx"
 import ProductCardSwiper from "../../Components/ProductCard/ProductCardSwiper.jsx"
 import productsData from "../../data/productsData.json"
-import '../../Components/ProductCard/ProductCard.css'
 import Header from '/src/Components/Header/Header.jsx'
 import Hero from "../../Components/Hero/Hero";
 import OfferCard from "../../Components/Offercards/Offer-cards.jsx";
