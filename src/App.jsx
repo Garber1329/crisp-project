9fa8731122560e9d82efb84d5d21179ca90e4d72
Vitalii Chaikovskyi <91221454+Garber1329@users.noreply.github.com>
2026-06-07 13:56:30 +0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import DashbordPage from "./pages/DashbordPage/DashbordPage.jsx";
 import CartPage from "./pages/CartPage/CartPage.jsx";
-import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx";
+import Register from "./Components/Register/Register.jsx";
 import ProductPage from "./pages/ProductPage/ProductPage.jsx";
-import LoginMain from "/src/Components/LoginPage/LoginPageContent.jsx";
+import LoginMain from "./Components/LoginPage/LoginPageContent.jsx";
 import { LoginContext } from "./Context/LoginContext.jsx";
 
 import ProductCatalog from "./pages/ProductCatalog/ProductCatalog.jsx";
 import Blog from "./Components/Blog/Blog.jsx";
-import { Routes, Route, Router } from 'react-router-dom'
 import Layout from './Components/Layout.jsx'
 import ShopPage from './pages/ShopPage/ShopPage.jsx'
 import SalePage from './pages/SalePge/SalePage.jsx'
@@ -26,8 +25,8 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route path='/register' element={<Register/>}/>
-          <Route path="/login" element={<LoginPageContent />} />
+          <Route path='/register' element={<Register />} />
+          <Route path="/login" element={<LoginMain />} />
           <Route path="/" element={<HomePage />} />
           <Route path='/shop' element={<ShopPage/>}/>
           <Route path="/dashboard" element={<DashbordPage />} />
