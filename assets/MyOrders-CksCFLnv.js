656e6b73b69d9cb12540323b00b041cadb7ad413
frotrest <125584696+frotrest@users.noreply.github.com>
2026-06-20 10:44:51 +0000

    Deploying to gh-pages from @ Garber1329/crisp-project@9de3ad0a40dfae0587722be2504d225be9d86dc7 🚀

--- assets/MyOrders-CksCFLnv.js
+++ assets/MyOrders-CksCFLnv.js
@@ -1,4 +1,4 @@
-import{y as s,r as c,a as x,j as e}from"./index-tv4YVoL0.js";const l=s.div`
+import{y as s,r as c,a as x,j as e}from"./index-CQPzC6x6.js";const l=s.div`
   padding: 20px;
 `,p=s.h2`
   margin-bottom: 30px;
---
+++ assets/SalePage-CZ1lJwF7.js
@@ -0,0 +1,1 @@
+import{c as o,j as r}from"./index-CQPzC6x6.js";import m from"./ProductPage-7FcN1OCg.js";function c(){const t=o.c(1);let e;return t[0]===Symbol.for("react.memo_cache_sentinel")?(e=r.jsx(r.Fragment,{children:r.jsx(m,{})}),t[0]=e):e=t[0],e}export{c as default};
